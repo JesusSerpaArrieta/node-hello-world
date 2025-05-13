5ad0add37dec7c7e2037664e9bee512aa3757235
Prespik <prespik@gmail.com>
2025-05-13 17:50:38 -0500

    Update for Jenkins Test 2

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-const http = require('http');
-
-const port = process.env.PORT || 3000;
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hello, World!\n");
-}).listen(port);
-
-console.log(`App is running... (port: ${port})`);
+console.log(`Hola mundo desde Node.JS`);
